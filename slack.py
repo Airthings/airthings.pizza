f00d496f037da512d8a1d914cea0e0d24dec008e
ocselvig <ole.selvig@airthings.com>
2022-03-10 15:40:19 +0100

    feat: get useres from channel instead of workplace

--- slack.py
+++ slack.py
@@ -8,7 +8,7 @@ slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
 def get_slack_users():
-    return sc.api_call("users.list")['members']
+    return sc.api_call("conversations.members")['members']
 
 def get_real_users(all_users):
     return [u for u in all_users if not u['deleted'] and not u['is_bot'] and not u['is_restricted'] and not u['name'] == "slackbot"] # type : list
